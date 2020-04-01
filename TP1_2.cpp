fa8f9036e8fdb40cfc546b4d2aba564c16ddaedf
Nicolás Arias <ariasmec06@gmail.com>
2020-03-31 22:20:00 -0300

    Se continua tp

--- TP1_2.cpp
+++ TP1_2.cpp
@@ -3,9 +3,12 @@
 
 int Cuadrado(int Num);
 void Cuadrado2(int a, int * cuad);
+void Invertir(int * a, int * b);
+
 int main(){
     int a;
     int Res;
+    int prim, seg;
    
     printf("Ingrese un numero\n");
     scanf("%d",&a);
@@ -15,6 +18,19 @@ int main(){
     Cuadrado2(a, &Res);
     printf("Elcuadrado del numerom metodo 2 es: %d\n", Res);
 
+    printf("\nEl valor de la variable es:%d",a);
+    printf("\nLa direccion de la variable es: %p", &a);
+
+    printf("\n\nIngrese el primer numero");
+    scanf("%d",&prim);
+    printf("\nInegrese el sedundo numero");
+    scanf("%d",&seg);
+
+    Invertir(&prim,&seg);
+    getchar();
+
+    printf("\n1: %d",prim);
+    printf("\n2: %d",seg);
 
 
     getchar();
@@ -32,6 +48,17 @@ int Cuadrado(int Num){
 void Cuadrado2(int a, int * cuad){
 
     *cuad= a * a;
+
+    return;
+}
+
+void Invertir(int *a, int *b){
+
+    int c;
+
+    c= *b;
+    *b= *a;
+    *a= c;
 
     return;
 }
